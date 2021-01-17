2d6b41f003453453628a373deaadd6231d4d086d
martynas3113 <martynas.pazusis@gmail.com>
2021-01-17 16:38:17 +0200

    Her osection added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './components/header/header';
+import Hero from './components/hero/hero';
   
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
